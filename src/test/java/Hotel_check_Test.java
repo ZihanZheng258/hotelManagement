6fa435e704f66b17caa26177178d30bc9eac0177
ziyongchen <14032635@studnet.uts.edu.au>
2021-10-02 21:25:26 +1000

    ziyong chen 3.2(origin azure )

--- src/test/java/Hotel_check_Test.java
+++ src/test/java/Hotel_check_Test.java
@@ -6,9 +6,14 @@ class Hotel_check_Test {
 
 	@Test
 	void test() {
-		assertNotNull(Hotel_check.hotel(123).getId());
+		assertEquals(Hotel_check.hotel(123).getId(),123);
 		assertNotNull(Hotel_check.hotel(123).getType());
 		assertNotNull(Hotel_check.hotel(123).getAddress());
+		assertEquals(Hotel_check.hotel(123).getArea(),123);
+		assertNotNull(Hotel_check.hotel(123).getIntroduction());
+		assertNotNull(Hotel_check.hotel(123).getName());
+		assertEquals(Hotel_check.hotel(123).getScore(),12);
+		assertEquals(Hotel_check.hotel(123).getStar(),12);
 	}
     
 }
