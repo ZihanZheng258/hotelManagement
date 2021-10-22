7ff83f357a722f620269a656fd0ba7e31b5e5734
Zihan Zheng <34792221+ZihanZheng258@users.noreply.github.com>
2021-10-22 16:33:41 +0800

    update DB delect password

--- src/main/java/dbc/DB.java
+++ src/main/java/dbc/DB.java
@@ -4,7 +4,7 @@ import java.sql.*;
 public class DB {
     protected String URL = "jdbc:mysql://localhost:3306/hotel?characterEncoding=utf8&useSSL=false&serverTimezone=UTC&rewriteBatchedStatements=true";
     protected String dbuser = "root";
-    protected String dbpass = "hjkk445998";
+    protected String dbpass = "";
     protected String driver = "com.mysql.cj.jdbc.Driver";
     protected Connection conn;
 }
