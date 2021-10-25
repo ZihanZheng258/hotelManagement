e2eef29bf333eb4c3e72d4e019c7fdd0282d2e26
yangkaiyuanznb <13480569@student.uts.edu.au>
2021-10-25 20:11:11 +0800

    add error check in search

--- src/main/java/Servlet/SearchHotelRecordServlet.java
+++ src/main/java/Servlet/SearchHotelRecordServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -20,16 +21,23 @@ public class SearchHotelRecordServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         String hotelType = req.getParameter("hotelType");
+        PrintWriter error = resp.getWriter();
 
         try {
             DaoManager manager = new DaoManager(new DBConnector().openConnection());
             HttpSession session = req.getSession();
             //调用查询方法,将List数据打包
             List<Hotel> hotelListByType = manager.hotel_find_by_type(hotelType);
-            System.out.println("show all" + hotelListByType.toString());
-            session.setAttribute("hotelListByType", hotelListByType);
-            //转发
-            req.getRequestDispatcher("/jsp/HotelRecordListByType.jsp").forward(req, resp);
+            if(hotelListByType.size()==0){
+                error.println("<script language='javascript'>alert('There is no such hotel type!! Please try again')</script>");
+                error.println("<script language='javascript'>window.location.href='/hotelManagement_war/jsp/HotelRecord.jsp'</script>");
+            }else{
+                System.out.println("show all" + hotelListByType.toString());
+                session.setAttribute("hotelListByType", hotelListByType);
+                //转发
+                req.getRequestDispatcher("/jsp/HotelRecordListByType.jsp").forward(req, resp);
+            }
+
 
 
         } catch (SQLException throwables) {
